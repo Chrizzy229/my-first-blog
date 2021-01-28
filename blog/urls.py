8e8a502e6b7382f6b7fc0812773ec6c2b7502105
Christina <christina.shepherd@taskaprosthetics.com>
2021-01-28 16:23:05 +1300

    Added view and template for adding a new blog post and it being added to the site

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # <int:pk> – Django expects an integer value and will transfer it to a view as a variable called pk
     # / – then we need a / again before finishing the URL.
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
