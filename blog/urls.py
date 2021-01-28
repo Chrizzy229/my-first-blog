a71f90aba82a42964bcc6b8cf6b6bf6d58b94c83
Christina <christina.shepherd@taskaprosthetics.com>
2021-01-28 15:54:26 +1300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    # post/ means that the URL should begin with the word post followed by a /. 
+    # <int:pk> – Django expects an integer value and will transfer it to a view as a variable called pk
+    # / – then we need a / again before finishing the URL.
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 # include the model we have written in models.py
 # The dot before models means current directory or current application. 
 from .models import Post
 
 # Create your views here.
+# one for each url
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
